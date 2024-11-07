63499190b027a848f3ee3635571d451c8d40dee0
Emilio Iacono <emilio.iacono@euris.it>
2024-11-07 11:31:04 +0100

    create blazor web app

--- ConsoleApplication.WebApp/Program.cs
+++ ConsoleApplication.WebApp/Program.cs
@@ -1,11 +1,14 @@
 using ConsoleApplication.Application;
 using ConsoleApplication.WebApp.Components;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.Scan(scan =>
 {
